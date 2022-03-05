d6bcd25568fdece800df422b5a844acf6fa38211
appearedherosheep <81283189+appearedherosheep@users.noreply.github.com>
2022-03-05 15:10:18 +0900

    feat: create-react-app

---
+++ cbsh-music/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjtqxplMQ83Uh4ZE55R6gIPUL6twJiVB4",
+  authDomain: "cbsh-musicdb.firebaseapp.com",
+  projectId: "cbsh-musicdb",
+  storageBucket: "cbsh-musicdb.appspot.com",
+  messagingSenderId: "624745236016",
+  appId: "1:624745236016:web:269401b66aca63890e8ff5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
