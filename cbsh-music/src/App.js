58f1dce9526dffbcd2cb3f0736a7f8e4c732b1ba
appearedherosheep <81283189+appearedherosheep@users.noreply.github.com>
2022-03-05 19:48:57 +0900

    feat : Simple To Do List

--- cbsh-music/src/App.js
+++ cbsh-music/src/App.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos.map((item, index) => (
+    <li key={index}>{item}</li>
+  )));
   return (
     <div>
-      <h4>hello world!</h4>
-
+      <h1>My To Dos({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
